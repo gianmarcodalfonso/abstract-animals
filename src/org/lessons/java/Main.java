0b717cd7739a9175684fbd52b4838679f1fe5582
gianmarcodalfonso <gianmarcoda10.gg@gmail.com>
2025-12-09 21:40:33 +0100

    feat: update Main class to include animal instances and method calls for Cane, Aquila, Passerotto, and Delfino

--- src/org/lessons/java/Main.java
+++ src/org/lessons/java/Main.java
@@ -1,5 +1,31 @@
 package org.lessons.java;
 
 public class Main {
+  public static void main(String[] args) {
+  Cane cane1 = new Cane("Charlie", 110, false);
+  Passerotto passerotto1 = new Passerotto("Ugo", 10, true);
+  Aquila aquila1 = new Aquila("Rah", 70, 230);
+  Delfino delfino1 = new Delfino("Birillo", 180, 55);
 
+
+  System.out.println("Cane");
+  cane1.dormi();
+  cane1.mangia();
+  cane1.verso();
+  
+  System.out.println("Aquila");
+  aquila1.dormi();
+  aquila1.mangia();
+  aquila1.verso();
+  
+  System.out.println("Passerotto");
+  passerotto1.dormi();
+  passerotto1.mangia();
+  passerotto1.verso();
+  
+  System.out.println("Delfino");
+  delfino1.dormi();
+  delfino1.mangia();
+  delfino1.verso();
+  }
 }
